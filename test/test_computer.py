e7d870f3dce1e893ad370bd00a4c5d90a08c1fcd
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2021-06-01 20:59:56 +0200

    Makes the test even less useful

--- test/test_computer.py
+++ test/test_computer.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 from src.features import computer
 
-def test_DeepThought():
+def test_importing_the_thinker():
     # Place where I will write a test "no dia de São Nunca!"
-    machine = computer.DeepThought(['abracadabra'])
-    assert True
+    #machine = computer.DeepThought(['abracadabra'])
+    pass
