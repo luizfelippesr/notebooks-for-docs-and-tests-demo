d6b6fb06b59cde18e30565982647384f5626facb
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2021-06-01 20:52:59 +0200

    Testing facilities

---
+++ test/test_computer.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('..')
+from src.features import computer
+
+def test_DeepThought():
+    # Place where I will write a test "no dia de São Nunca!"
+    machine = computer.DeepThought(['abracadabra'])
+    assert True
